4ae5b5a5a3613b045641fab0e6d4693581b102af
wzmzero <1772009366@qq.com>
2025-08-01 14:07:23 +0800

    modbus test

---
+++ test_dirver_v1/build/obj/tests/test_endpoit.d
@@ -0,0 +1,11 @@
+build/obj/tests/test_endpoit.o: tests/test_endpoit.cpp \
+ inc/tcp_server_endpoint.h inc/endpoint.h inc/logrecord.h \
+ inc/tcp_client_endpoint.h inc/udp_server_endpoint.h \
+ inc/udp_client_endpoint.h inc/serial_endpoint.h
+inc/tcp_server_endpoint.h:
+inc/endpoint.h:
+inc/logrecord.h:
+inc/tcp_client_endpoint.h:
+inc/udp_server_endpoint.h:
+inc/udp_client_endpoint.h:
+inc/serial_endpoint.h:
---
+++ test_dirver_v2/build/obj/src/config_parser.d
@@ -0,0 +1,4 @@
+build/obj/src/config_parser.o: src/config_parser.cpp inc/config_parser.h \
+ inc/shared_structs.h
+inc/config_parser.h:
+inc/shared_structs.h:
